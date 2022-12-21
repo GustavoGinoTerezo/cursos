90d1ec875759d39bc6e49b9d99d96efd0b552354
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-21 20:33:38 -0300

    Criado o Primeiro Programa

---
+++ Java_Foundations/Seção_0_Recursos_do_curso/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package Java_Foundations.Seção_0_Recursos_do_curso;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
