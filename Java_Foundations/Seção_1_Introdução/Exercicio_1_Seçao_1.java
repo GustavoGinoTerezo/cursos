3cf146e7fdd3ab5efec28b6e3d2705b4d7075a21
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-22 21:59:41 -0300

    Seção 1 - Exercício 1

---
+++ Java_Foundations/Seção_1_Introdução/Exercicio_1_Seçao_1.java
@@ -0,0 +1,19 @@
+package Java_Foundations.Seção_1_Introdução;
+
+public class Exercicio_1_Seçao_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Esse é um exercício para alterar o printo do Hello World por está linha.");
+
+        /*
+         * system.out.println(); -> Esse é outro teste do exercício em que o mesmo
+         * solicita para alterar a letra S de System por uma minúscula, resultando em um
+         * erro na execução.
+         * 
+         * System.out.println() -> Nesse teste foi solicitado para retirar o ponto e
+         * virgula do comando, resultando em um erro na execução
+         */
+    }
+
+}
