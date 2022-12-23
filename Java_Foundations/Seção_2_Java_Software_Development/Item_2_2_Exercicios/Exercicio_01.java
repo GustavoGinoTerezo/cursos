928a3d9b4c7d2abdbfe981fa265f997943b2efb4
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-22 22:26:33 -0300

    Seção 2 - Exercícios

---
+++ Java_Foundations/Seção_2_Java_Software_Development/Item_2_2_Exercicios/Exercicio_01.java
@@ -0,0 +1,16 @@
+package Java_Foundations.Seção_2_Java_Software_Development.Item_2_2_Exercicios;
+
+public class Exercicio_01 {
+    
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ "); 
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+    //DEFINIDO PONTO DE INTERRUPÇÃO NAS LINHAS 6, 9, 13 CONFORME SOLICITADO NO EXERCÍCIO
+}
---
+++ Java_Foundations/Seção_2_Java_Software_Development/Item_2_2_Exercicios/Exercicio_02.java
@@ -0,0 +1,5 @@
+package Java_Foundations.Seção_2_Java_Software_Development.Item_2_2_Exercicios;
+
+public class Exercicio_02 {
+    
+}
