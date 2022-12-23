504c4c20f4c8bd80af209873bc0178bdb0862d97
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-22 22:31:36 -0300

    Update Exercicio_02.java

--- Java_Foundations/Seção_2_Java_Software_Development/Item_2_2_Exercicios/Exercicio_02.java
+++ Java_Foundations/Seção_2_Java_Software_Development/Item_2_2_Exercicios/Exercicio_02.java
@@ -1,5 +1,24 @@
 package Java_Foundations.Seção_2_Java_Software_Development.Item_2_2_Exercicios;
 
 public class Exercicio_02 {
-    
+
+    public static void main(String[] args) {
+        /*
+         * System.out.println("1");
+         * {
+         * System.out.println("2")
+         * }
+         * System.out.println("3");
+         * }
+         * System.out.println ("4");
+         * }
+         */
+
+        // EXERCÍCIO PARA ARRUMAR O CÓDIGO ACIMA E FAZE-LÔ FUNCIONAR
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
 }
