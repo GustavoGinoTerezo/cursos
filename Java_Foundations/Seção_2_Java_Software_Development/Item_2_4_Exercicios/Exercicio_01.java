b7490112f515e2b48dc5a32b43a8835b2ff3f6d7
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-23 21:01:48 -0300

    Criado - Exercício_01

---
+++ Java_Foundations/Seção_2_Java_Software_Development/Item_2_4_Exercicios/Exercicio_01.java
@@ -0,0 +1,21 @@
+package Java_Foundations.Seção_2_Java_Software_Development.Item_2_4_Exercicios;
+
+public class Exercicio_01 {
+    
+    public static void main(String[] args) {
+        
+        //Use 8 prints para printar uma carinha feliz.
+        //Sua arte deve ser feita com somente um tipo de caractere, podendo ser X ou #
+        
+        System.out.println("  ####  ");
+        System.out.println(" #    # ");
+        System.out.println("# #  # #");
+        System.out.println("#      #");
+        System.out.println("# #  # #");
+        System.out.println("#  ##  #");
+        System.out.println(" #    # ");
+        System.out.println("  ####  ");
+
+    }
+
+}
