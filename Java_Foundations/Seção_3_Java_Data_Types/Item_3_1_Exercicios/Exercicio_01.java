c6bd29f58d51167778982953b18880bf3f974792
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-24 20:55:26 -0300

    Criado - Exercício_01

---
+++ Java_Foundations/Seção_3_Java_Data_Types/Item_3_1_Exercicios/Exercicio_01.java
@@ -0,0 +1,46 @@
+package Java_Foundations.Seção_3_Java_Data_Types.Item_3_1_Exercicios;
+
+public class Exercicio_01 {
+    
+    public static void main(String[] args) {
+
+        /*
+         * SOLICITADO NO EXERCÍCIOS QUE O PROGRAMA FOSSE CORRIGIDO POIS HAVIA ALGUNS PROBLEMAS
+         * NO MESMO, FAZENDO COM QUE NÃO FOSSE POSSÍVEL SER EXECUTADO.
+         * CONFORME O CÓDIGO ABAIXO, TODAS AS LINHAS QUE POSSUEM COMENTÁRIO, SÃO OS ERROS ACHA-
+         * DOS NO CÓDIGO.
+         */
+
+        boolean bool = true;
+
+        //int intVar1 = true;
+        int intVar1 = 1;
+        int intVar2 = 2;
+        //intVar3 = 3;
+        int intVar3 = 3;
+
+        double doubleVar1, doubleVar2, doubleVar3, doubleVar4;
+        doubleVar1 = 1.1;
+        doubleVar2 = 2.1;
+        //double doubleVar3 = 3.1;
+        doubleVar3 = 3.1;
+        doubleVar4 = 4.1; // NÃO HAVIA ESSA LINHA
+
+        String stringVar1 = "1";
+        //String stringVar1 = 1;
+        String stringVar2 = "2";
+
+        // Don't edit these print statements
+        System.out.println("bool = " + bool);
+        System.out.println("intVar1 = " + intVar1);
+        System.out.println("intVar2 = " + intVar2);
+        System.out.println("intVar3 = " + intVar3);
+        System.out.println("doubleVar1 = " + doubleVar1);
+        System.out.println("doubleVar2 = " + doubleVar2);
+        System.out.println("doubleVar3 = " + doubleVar3);
+        System.out.println("doubleVar4 = " + doubleVar4);
+        System.out.println("stringVar1 = " + (stringVar1 + 1));
+        System.out.println("stringVar2 = " + (stringVar2 + 2));
+    }
+
+}
