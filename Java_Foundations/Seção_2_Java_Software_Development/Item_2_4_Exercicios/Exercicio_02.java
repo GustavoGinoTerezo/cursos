0355a23fb7129315ac6fe53d402a0532885d1dff
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-23 21:02:21 -0300

    Criado Exercicio_02

---
+++ Java_Foundations/Seção_2_Java_Software_Development/Item_2_4_Exercicios/Exercicio_02.java
@@ -0,0 +1,26 @@
+package Java_Foundations.Seção_2_Java_Software_Development.Item_2_4_Exercicios;
+
+public class Exercicio_02 {
+
+    public static void main(String[] args) {
+        // Use o comando print para criar sua própria arte no modelo ASCII
+
+        System.out.println("#      #         #           ##");
+        System.out.println("##     #        # #         #  #");
+        System.out.println("# #    #       #   #       #    #");
+        System.out.println("#  #   #      #     #     #      #");
+        System.out.println("#   #  #     #########    #      #");
+        System.out.println("#    # #    #         #    #    #");
+        System.out.println("#     ##   #           #    #  #");
+        System.out.println("#      #  #             #    ##");
+        System.out.println("");
+        System.out.println(" ###   #####  ##");
+        System.out.println("#   #  #   #  ##");
+        System.out.println("#      #        ");
+        System.out.println(" ###   ####   ##");
+        System.out.println("   #   #      ##");
+        System.out.println("#   #  #      ##");
+        System.out.println("#   #  #   #  ##");
+        System.out.println(" ###   #####  ##");
+
+}
