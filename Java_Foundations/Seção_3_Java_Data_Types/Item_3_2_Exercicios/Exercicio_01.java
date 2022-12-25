2d874d2f572616554d569e2a7a8c6e55294d6ab8
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-25 13:36:32 -0300

    Update Exercicio_01.java

--- Java_Foundations/Seção_3_Java_Data_Types/Item_3_2_Exercicios/Exercicio_01.java
+++ Java_Foundations/Seção_3_Java_Data_Types/Item_3_2_Exercicios/Exercicio_01.java
@@ -4,31 +4,29 @@ import javax.sound.sampled.SourceDataLine;
 import javax.swing.JOptionPane;
 
 public class Exercicio_01 {
-    
-    
-    public static void somaOvosV1(){
 
-    int chickenCount = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de galinhas: "));
-    int newChicken = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de galinhas que foram adicionadas: "));;
-    int newChickenCount = chickenCount+newChicken;
-    int endChickenCount = newChickenCount/2;
-    int eggsPerChicken = Integer.parseInt(JOptionPane.showInputDialog("Qual a quantidade de ovos por galinhas: "));;
-    
-    int segunda = chickenCount*eggsPerChicken;
-    int terça = newChickenCount*eggsPerChicken;
-    int quarta = endChickenCount*eggsPerChicken;
+    public static void somaOvos() {
 
-    int somaOvos = segunda + terça + quarta ;
-    
-    System.out.println(somaOvos);
+        int chickenCount = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de galinhas: "));
+        int newChicken = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de galinhas que foram adicionadas: "));
+        int newChickenCount = chickenCount + newChicken;
+        int endChickenCount = newChickenCount / 2;
+        int eggsPerChicken = Integer.parseInt(JOptionPane.showInputDialog("Qual a quantidade de ovos por galinhas: "));
+        
+        int segunda = chickenCount * eggsPerChicken;
+        int terça = newChickenCount * eggsPerChicken;
+        int quarta = endChickenCount * eggsPerChicken;
+
+        int somaOvos = segunda + terça + quarta;
+
+        System.out.println(somaOvos);
 
     }
 
-    
     public static void main(String[] args) {
-    
-        Exercicio_01.somaOvosV1();
-    
+
+        Exercicio_01.somaOvos();
+
     }
-    
+
 }
