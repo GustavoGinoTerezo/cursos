3597620a05dae61b26dba1a442899d624f60bfad
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-24 21:09:26 -0300

    Criado - Exercício_01
    Terminando

---
+++ Java_Foundations/Seção_3_Java_Data_Types/Item_3_2_Exercicios/Exercicio_01.java
@@ -0,0 +1,33 @@
+package Java_Foundations.Seção_3_Java_Data_Types.Item_3_2_Exercicios;
+
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.JOptionPane;
+
+public class Exercicio_01 {
+    
+    /*
+    public static void somaOvosV1(){
+
+    int chickenCount = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de galinhas: "));
+    int newChickenCount = chickenCount+1;
+    int endChickenCount = newChickenCount/2;
+    int eggsPerChicken = 5;
+    
+    int segunda = chickenCount*eggsPerChicken;
+    int terça = newChickenCount*eggsPerChicken;
+    int quarta = endChickenCount*eggsPerChicken;
+
+    int somaOvos = segunda + terça + quarta ;
+    
+    System.out.println(somaOvos);
+
+    }
+
+    
+    public static void main(String[] args) {
+    
+        Exercicio_01.somaOvosV1();
+    
+    }
+    */
+}
