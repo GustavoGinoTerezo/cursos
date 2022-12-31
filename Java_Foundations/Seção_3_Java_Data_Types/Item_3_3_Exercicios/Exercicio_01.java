5a3bf2a7f48775e59d163139a0828fee749341ea
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-31 18:33:50 -0300

    Update Exercicio_01.java

--- Java_Foundations/Seção_3_Java_Data_Types/Item_3_3_Exercicios/Exercicio_01.java
+++ Java_Foundations/Seção_3_Java_Data_Types/Item_3_3_Exercicios/Exercicio_01.java
@@ -1,5 +1,7 @@
 package Java_Foundations.Seção_3_Java_Data_Types.Item_3_3_Exercicios;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_01 {
 
     /*
@@ -10,8 +12,12 @@ public class Exercicio_01 {
 
         String nome = "Gustavo";
         String item = "camiseta";
-        String valor = "R$ 30,00";
-        String descriçao = nome + " quer comprar uma " + item + ". O valor do produto custa " + valor + ".";
+        double valor = 30.00;
+        double taxa = 1.05;
+        int quantidade = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual a quantidade de camisetas que serão compradas: "));
+        double valorFinal = valor*taxa*quantidade;
+
+        String descriçao = nome + " quer comprar " + quantidade + " " + item + ". O valor total é igual a R$ " + valorFinal;
 
         System.out.println(descriçao);
 
