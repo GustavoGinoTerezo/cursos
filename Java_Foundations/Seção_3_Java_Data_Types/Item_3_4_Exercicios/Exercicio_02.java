e46f9b2828e9932e0f92d76a9c2731d3982738de
GustavoGinoTerezo <gustavogino@outlook.com.br>
2022-12-31 19:24:20 -0300

    Criado - Exercicio_02

--- Java_Foundations/Seção_3_Java_Data_Types/Item_3_4_Exercicios/Exercicio_02.java
+++ Java_Foundations/Seção_3_Java_Data_Types/Item_3_4_Exercicios/Exercicio_02.java
@@ -1,27 +1,38 @@
 package Java_Foundations.Seção_3_Java_Data_Types.Item_3_4_Exercicios;
 
+import java.text.DecimalFormat;
+
 public class Exercicio_02 {
+
+    /*
+     * EXERCICIO FEITO PARA TREINAMENTO DE USO DE FORMATAÇÃO DE NÚMEROS E
+     * ALTERAÇÃO DE TIPOS PRIMITIVOS COM SEUS RESPECTIVOS INTERVALOS.
+     */
 
     public static void main(String[] args) {
 
         // POPULAÇÃO MUNDIAL ATUALMENTE
         double populaçaoAtualMundo = 7_000_000_000.00;
-        System.out.println("POPULAÇÃO MUNDIAL ATUALMENTE: " + populaçaoAtualMundo);
+        String popMundialFormatado = new DecimalFormat("#,##0").format(populaçaoAtualMundo);
+        System.out.println("POPULAÇÃO MUNDIAL ATUALMENTE: " + popMundialFormatado);
 
         // POPULAÇÃO MUNDIAL EM 1950, Source: United Nations Population Division
         // 2.52 Bilhões
-        int africaPop = 221_000_000;
-        int asiaPop = 1_402_000_000;
-        int europePop = 547_000_000;
-        int americaPop = 339_000_000;
-        int oceaniaPop = 13_000_000;
+        double africaPop = 221_000_000;
+        double asiaPop = 1_402_000_000;
+        double europePop = 547_000_000;
+        double americaPop = 339_000_000;
+        double oceaniaPop = 13_000_000;
+        double soma = africaPop + asiaPop + europePop + americaPop + oceaniaPop;
+        String valorFormatado = new DecimalFormat("#,##0").format(soma);
         System.out.println("POPULAÇÃO MUNDIAL EM 1950: "
-                + africaPop + asiaPop + europePop + americaPop + oceaniaPop);
+                + valorFormatado);
 
         // A POPULAÇÃO ATUAL DA ÁSIA É EQUIVALENTE A 60% DA POPULAÇÃO MUNDIAL.
         double porcentagemAsia = 0.6;
         double populaçaoAsiaAtual = populaçaoAtualMundo * porcentagemAsia;
-        System.out.println("POPULAÇÃO ATUAL NA ÁSIA: " + populaçaoAsiaAtual);
+        String popAsiaFormatado = new DecimalFormat("#,##0").format(populaçaoAsiaAtual);
+        System.out.println("POPULAÇÃO ATUAL NA ÁSIA: " + popAsiaFormatado);
 
     }
 
