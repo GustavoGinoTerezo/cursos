8334884e04d937045791f28708f4d36528f4110f
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-03-15 19:10:12 -0300

    Exercício_04

---
+++ Python_Cisco/Modulo_4/Seçao_4.3/Exercicio_04.py
@@ -0,0 +1,14 @@
+# EXERCÍCIO DE CHECAGEM DE NÚMERO PRIMO, ONDE O MESMO É VERIFICADO DENTRO DA FUNÇÃO "NUMERO_PRIMO"
+# O RANGE DE "NÚMERO_PRIMO É DEFINIDO DE MODO QUE O MESMO SEJA PEQUENO, FAZENDO COM QUE NÃO HAJA
+# VÁRIOS TESTES DENTRO DA ESTRUTURA DE REPETIÇÃO, E COM ISSO É FEITA A VALIDAÇÃO AUTOMATICAMENTE
+
+def numero_primo(num):
+    for i in range(2, int(1 + num ** 0.5)):
+        if num % i == 0:
+            return False
+        return True
+
+
+for i in range(90, 100):
+    if numero_primo(i):
+        print(i, end=" ")
