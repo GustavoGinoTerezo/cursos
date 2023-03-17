103bab6e2db11ad0e989c9ed4addd141f613c7ac
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-03-17 17:46:36 -0300

    Exercício_003

---
+++ Python_Curso_em_Video/Exercicios/ex003.py
@@ -0,0 +1,6 @@
+num1 = int(input("Escolha um valor inteiro: "))
+num2 = int(input("Escolha outro valor inteiro: "))
+soma = (num1 + num2)
+
+print("A soma do primeiro número ({}), e do segundo número ({}), é igual a {}.".format(
+    num1, num2, soma))
