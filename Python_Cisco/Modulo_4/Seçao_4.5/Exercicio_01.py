2e65f3f8a8c97b43110ef871fc9949049003de81
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-03-22 17:02:06 -0300

    Update Exercicio_01.py

--- Python_Cisco/Modulo_4/Seçao_4.5/Exercicio_01.py
+++ Python_Cisco/Modulo_4/Seçao_4.5/Exercicio_01.py
@@ -5,6 +5,9 @@
 #   -INPUT
 
 def imc(peso, altura):
+    if altura < 1.0 or altura > 2.5 or peso < 20 or peso > 200:
+        print (None)
+        return None
     resultado = peso/altura**2
     resultado_format = "{:.2f}".format(resultado)
     print("O valor do seu IMC é igual a:", resultado_format)
