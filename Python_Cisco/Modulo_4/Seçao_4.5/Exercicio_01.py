8e69524e483afab9c8d684d7712bbd5f1505b974
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-03-22 17:19:36 -0300

    Update Exercicio_01.py

--- Python_Cisco/Modulo_4/Seçao_4.5/Exercicio_01.py
+++ Python_Cisco/Modulo_4/Seçao_4.5/Exercicio_01.py
@@ -6,14 +6,29 @@
 
 def imc(peso, altura):
     if altura < 1.0 or altura > 2.5 or peso < 20 or peso > 200:
-        print (None)
+        print(None)
         return None
     resultado = peso/altura**2
     resultado_format = "{:.2f}".format(resultado)
     print("O valor do seu IMC é igual a:", resultado_format)
 
 
-peso = float(input("Qual o seu peso em kilogramas: "))
-altura = float(input("Qual a sua altura em metros: "))
+def libras_para_kilos(lb):
+    return lb * 0.45359237
+    # 1 lb = 0.45359237 kg
 
-imc(peso, altura)
+
+def imc_verifica_peso():
+    verificar_peso = int(input("Digite a opção:\n1 - Peso em Libras\n2 - Peso em kg"))
+    if verificar_peso == 1:
+        libras = int(input("Qual o seu peso em libras: "))
+        result_libras = libras_para_kilos(libras)
+        altura = float(input("Qual a sua altura em metros: "))    
+        imc(result_libras,altura)
+        
+
+def imc_print():
+    peso = float(input("Qual o seu peso em kilogramas: "))
+    altura = float(input("Qual a sua altura em metros: "))
+    imc(peso, altura)
+
