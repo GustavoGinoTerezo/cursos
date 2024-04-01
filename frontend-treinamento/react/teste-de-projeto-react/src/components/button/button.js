fb57a24c56de17a1a76bd7634fd3eb9d12bd63f7
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2024-04-01 20:21:16 -0300

    Update

---
+++ frontend-treinamento/react/teste-de-projeto-react/src/components/button/button.js
@@ -0,0 +1,14 @@
+import './button.css'
+
+function MyButton() {
+  return (
+
+    <div className="teste">
+        <button>
+          I'm a button
+        </button>
+    </div>
+  );
+}
+
+export default MyButton;
