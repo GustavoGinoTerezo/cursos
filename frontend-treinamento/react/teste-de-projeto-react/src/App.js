c1a02d3d57ac27edc193497019e0a6345a92a063
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2024-05-06 21:09:03 -0300

    Update

--- frontend-treinamento/react/teste-de-projeto-react/src/App.js
+++ frontend-treinamento/react/teste-de-projeto-react/src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
 import MyButton from './components/button/button.js';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-        <br/>
-        <button>Testando botão</button>
-        
-      </header>
-      <div><MyButton/></div>
+      
     </div>
    
   );
