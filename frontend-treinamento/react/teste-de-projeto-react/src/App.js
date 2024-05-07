0c10b9b4ccf88de256be7bbcab63839c392b8c88
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2024-05-07 03:26:12 -0300

    Update

--- frontend-treinamento/react/teste-de-projeto-react/src/App.js
+++ frontend-treinamento/react/teste-de-projeto-react/src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 
-import MyButton from './components/button/button.js';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+
+import About from './pages/about'
+import Aside from '../src/pages/components/aside';
+import ContactPages from './pages/contactPages'
+import Footer from '../src/pages/components/footer';
+import Header from '../src/pages/components/header';
+import Home from './pages/home';
+import Service from './pages/services'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className="displayFlex">
+        <Header />
+        <main>
+          <Aside />
+          <article>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/servicos" element={<Service />} />
+            <Route path="/contato" element={<ContactPages />} />
+          </Routes>
+          </article>
+        </main>
+      </div>
+      <Footer />
     </div>
-   
   );
 }
 
